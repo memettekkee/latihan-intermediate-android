69c64ea3bb051026ba7311860e6dd2590343812a
Muhammad <muhammadahjahh@gmail.com>
2024-05-01 13:50:04 +0700

    ticketing apps with custom view

---
+++ TicketingApps/app/src/main/java/com/dicoding/ticketingapps/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.dicoding.ticketingapps
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.ticketingapps.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val seatView = findViewById<SeatView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+}
