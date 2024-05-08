b59938abb747fb9e1df9d70ea807d0692040b5fb
Muhammad <muhammadahjahh@gmail.com>
2024-05-08 09:39:39 +0700

    open gallery, camera, and custom camera

--- MyService/app/src/main/java/com/dicoding/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/dicoding/myservice/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.dicoding.myservice
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.MyBackgroundService
+import com.MyForegroundService
 import com.dicoding.myservice.databinding.ActivityMainBinding
+import android.app.Service
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean? ->
+        if (!isGranted!!)
+            Toast.makeText(this,
+                "Unable to display Foreground service notification due to permission decline",
+                Toast.LENGTH_LONG)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED)
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener {
@@ -24,6 +50,17 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopBackgroundService.setOnClickListener {
             stopService(serviceIntent)
+        }
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
     }
 }
